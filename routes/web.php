38ac1ab15cf46efaab6967dbacec6f42e3208977
Kathlyn <kaibo@my.cspc.edu.ph>
2025-09-08 01:20:57 +0800

    change to php the html  and had a path in routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Home', function () {
+    return view('Home');
+});
+
+Route::get('/About', function () {
+    return view('About');
+});
+
+Route::get('/Contact', function () {
+    return view('Contact');
+});
+
+
+
