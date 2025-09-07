1f2ed291fd3c0ce7ab1b363e04aa355f94557eb7
Kathlyn <kaibo@my.cspc.edu.ph>
2025-09-08 03:31:08 +0800

    template for home page

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+
+     <link rel="stylesheet" href="{{asset('css/home.css')}}">
+
+     
+
+
+   
+</head>
+<body class="bg-light text-center p-5">
+
+    @yield('content')
+
+
+    @yield('scripts')
+    <!-- Bootstrap JS (CDN) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    
+</body>
+</html>
